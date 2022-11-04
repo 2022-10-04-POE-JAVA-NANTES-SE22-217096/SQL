9e04b57fa5fdf35f91367d918a155143dc704dc6
AllanBernier <allan.bernier1@gmail.com>
2022-11-04 17:08:13 +0100

    Fix

--- CERTIF/create.sql
+++ CERTIF/create.sql
@@ -1,6 +1,13 @@
 
 
 
+
+
+
+
+
+
+
 DROP TABLE user_channel;
 DROP TABLE messages;
 DROP TABLE users;
@@ -9,15 +16,15 @@ DROP TABLE canaux;
 
 CREATE TABLE canaux(
     id INT(20) PRIMARY KEY AUTO_INCREMENT,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     description TEXT,
     name VARCHAR(255) NOT NULL,
-    updated_at TIMESTAMP, 
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
 );
 
 CREATE TABLE roles(
     id INT(20) PRIMARY KEY AUTO_INCREMENT,
-    role VARCHAR(255),
+    role VARCHAR(255)
 );
 
 CREATE TABLE users(
@@ -26,18 +33,18 @@ CREATE TABLE users(
     password VARCHAR(50) NOT NULL,
     username VARCHAR(100) NOT NULL,
     role_id INT(20),
-    CONSTRAINT fk_user_role FOREIGN KEY (role_id) REFERENCES roles(id),
+    CONSTRAINT fk_user_role FOREIGN KEY (role_id) REFERENCES roles(id)
 );
 
 CREATE TABLE messages(
     id INT(20) PRIMARY KEY AUTO_INCREMENT,
     content TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP, 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
     canal_id INT(20),
     user_id INT(20),
     CONSTRAINT fk_message_canal FOREIGN KEY (canal_id) REFERENCES canaux(id),
-    CONSTRAINT fk_message_user FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT fk_message_user FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE user_channel(
@@ -45,16 +52,8 @@ CREATE TABLE user_channel(
     canal_id INT(20),
     user_id INT(20),
     CONSTRAINT fk_user_canal FOREIGN KEY (canal_id) REFERENCES canaux(id),
-    CONSTRAINT fk_canal_user FOREIGN KEY (user_id) REFERENCES users(id),
-
+    CONSTRAINT fk_canal_user FOREIGN KEY (user_id) REFERENCES users(id)
 );
-
-
-
-
-
-
-
 
 
 
---
+++ CERTIF/query.sql
@@ -0,0 +1,40 @@
+--Vous devez insérer des valeurs avant mais si vous avez de l'avance voici les requêtes à exécuter cet après-midi
+--1. Rechercher tous les utilisateurs du canal général.
+SELECT users.*
+FROM canaux, user_channel, users
+WHERE user_channel.canal_id = canaux.id
+AND user_channel.user_id = users.id
+AND canaux.name = "général";
+
+
+--2. Rechercher tous les messages du canal général trié par date de création
+
+SELECT messages.*
+FROM canaux, messages
+WHERE messages.canal_id = canaux.id
+AND canaux.name = "général"
+ORDER BY messages.created_at;
+
+--3. Rechercher tous les messages d'un utilisateur
+SELECT messages.* 
+FROM messages, users
+WHERE messages.user_id = users.id
+AND users.username = "mysql";
+
+--4. Rechercher tous les messages d'un utilisateur sur un canal
+SELECT messages.* 
+FROM messages, users, canaux
+WHERE messages.user_id = users.id
+AND messages.canal_id = canaux.id
+AND users.username = "mysql"
+AND canal.name = "général";
+
+--5. Rechercher les noms des utilisateurs ayant un mot de passe en commun
+
+--6. Rechercher les utilisateurs ayant "java" dans leurs description
+--7. Recherche tous les messages parlant de m2i
+--8. Rechercher le nom de tous les utilisateurs ayant parlé de m2i
+--9. Rechercher le premier canal créait
+--10. Rechercher le premier message envoyé
+--11. Rechercher tous les utilisateurs ayant un canal en commun avec l'utilisateur 1.
+--12. Rechercher tous les mots de passe de moins de 8 caractères
